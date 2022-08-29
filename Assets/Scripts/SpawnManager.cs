5b5a87d8bf7faddee3c7c5d75b7b45efd34ab38a
H-JungJaeHeon <jjhking222@naver.com>
2022-08-30 01:33:42 +0900

    EnemySpawnMake
    EnemySpawnMake

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,6 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SpawnDirection
+{
+    Down,
+    Left,
+    Right,
+    Up
+}
+
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
@@ -16,11 +24,9 @@ public class SpawnManager : MonoBehaviour
     [Tooltip("시간당 소환될 적 오브젝트")]
     private GameObject enemyObj;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    [Tooltip("소환 위치")]
+    private GameObject[] enemySpawnerObjs;
 
     // Update is called once per frame
     void Update()
@@ -35,8 +41,16 @@ public class SpawnManager : MonoBehaviour
             nowSpawnCoolTime += Time.deltaTime;
             if (nowSpawnCoolTime >= maxSpawnCoolTime)
             {
+                int randSpawnerIndex = Random.Range(0, 4);
+                Vector3 spawnRangeColliderSize = enemySpawnerObjs[randSpawnerIndex].GetComponent<BoxCollider>().size;
+                Vector3 spawnRangeColliderCenter = enemySpawnerObjs[randSpawnerIndex].GetComponent<BoxCollider>().center;
+
+                float posX = enemySpawnerObjs[randSpawnerIndex].transform.position.x + Random.Range(-spawnRangeColliderSize.x / 2f, spawnRangeColliderSize.x / 2f);
+                float posZ = enemySpawnerObjs[randSpawnerIndex].transform.position.z + Random.Range(-spawnRangeColliderSize.z / 2f, spawnRangeColliderSize.z / 2f);
+
+                print(new Vector3(posX, 0, posZ));
+                Instantiate(enemyObj, new Vector3(spawnRangeColliderCenter.x + posX, 0, spawnRangeColliderCenter.z + posZ), Quaternion.identity);
                 nowSpawnCoolTime = 0;
-                Instantiate(enemyObj, transform.position, Quaternion.identity);
             }
         }
     }
